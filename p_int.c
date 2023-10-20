ddafe03499c4e502c0e3ce0f0bea6f8bac23c4b9
Maso <nkombisa.masonwabe@gmail.com>
2023-10-20 11:48:27 +0200

    mod for n<0

--- p_int.c
+++ p_int.c
@@ -14,9 +14,9 @@ int p_int(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		count = p_int(n * -1);
 	}
-	if (n / 10)
+	if (n / 10 && n > 0)
 		count = p_int(n / 10);
 
 	digit = (n % 10) + '0';
